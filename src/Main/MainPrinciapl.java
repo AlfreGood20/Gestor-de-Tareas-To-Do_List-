f9e4e1f828a10e4cd753d16d71148747bb2ec7a0
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2025-01-21 16:50:22 -0600

    Donde se ejecutara el programa

--- src/Main/MainPrinciapl.java
+++ src/Main/MainPrinciapl.java
@@ -1,9 +1,10 @@
 package Main;
+import VentanaBienvenida.JframeVentana;
 
 public class MainPrinciapl {
-
+	
+	 //cd "C:\Users\HP\Documents\PROGRAMACION\Gestor De Tareas(Proyecto java)"
 	public static void main(String[] args) {
-		
+		new JframeVentana().setVisible(true);; // INICIA LA VENTANA BIENVENIDO
 	}
-
 }
